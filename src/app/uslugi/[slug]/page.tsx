56891b16b49590ff932e85730a2086a1f2f2d47a
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-08-23 15:07:36 +0200

    Fix for the services menu links

--- src/app/uslugi/[slug]/page.tsx
+++ src/app/uslugi/[slug]/page.tsx
@@ -1,6 +1,13 @@
 export const dynamicParams = false;
 
-import { getAllServices, getServiceBySlug } from "@/lib/jetApi";
+import SectionHero from "@/components/SectionHero";
+import TargetAudienceSection from "@/components/Sections/TargetAudienceSection";
+import {
+	getAllServices,
+	getServiceBySlug,
+	getServiceImages,
+	mapImagesWithWordPressAlt,
+} from "@/lib/jetApi";
 
 export async function generateStaticParams() {
 	const services = await getAllServices();
@@ -9,9 +16,19 @@ export async function generateStaticParams() {
 
 async function Page({ params }: { params: { slug: string } }) {
 	const { slug } = await params;
-
-	// const service = await getServiceBySlug(slug);
-	return <p>{slug}</p>;
+	const service = await getServiceBySlug(slug);
+	const { id, meta, title } = service;
+	const images = await mapImagesWithWordPressAlt(getServiceImages(service));
+	console.log(images);
+	return (
+		<>
+			<SectionHero
+				subTitle={meta.hero_subheadline}
+				title={meta.hero_h1}
+				description={meta.hero_intro}
+			></SectionHero>
+		</>
+	);
 }
 
 export default Page;
