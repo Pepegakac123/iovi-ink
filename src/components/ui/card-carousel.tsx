7cdfb34b500e63feea0c3da53a3dec0291aa66c0
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-08-23 12:19:36 +0200

    Fixed Gallery Resolution

--- src/components/ui/card-carousel.tsx
+++ src/components/ui/card-carousel.tsx
@@ -295,11 +295,11 @@ export const CardCarousel: React.FC<CarouselProps> = ({
 											src={image.src}
 											alt={image.alt}
 											fill
-											sizes="(max-width:480px) 250px, (max-width:768px) 280px, (max-width:1400px) 420px, 560px"
+											sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, (max-width: 1280px) 33vw, 25vw"
 											style={{ objectFit: "cover", objectPosition: "center" }} // lub "center top"
 											priority={index < 6}
 											loading={index < 6 ? "eager" : "lazy"}
-											quality={90}
+											quality={100}
 										/>
 									</div>
 								</div>
